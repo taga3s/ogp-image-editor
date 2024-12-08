5391286758230c1b5e581a9cbe7bc0fa0ed58a7b
taga3s <bepozurapu274@gmail.com>
2024-12-08 16:03:21 +0900

    initial setup

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header class="px-4 py-5 border-b-2">
+      <div class="flex items-end gap-4">
+        <span class="text-2xl font-bold">OGP画像めーかー</span>
+        <span>
+          Created by{" "}
+          <a href="https://github.com/taga3s" class="text-cyan-600 underline">
+            taga3s
+          </a>
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
