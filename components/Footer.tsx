0e0ba4bcdf4dd07ee7daf055419401674d38e4eb
taga3s <bepozurapu274@gmail.com>
2024-12-22 09:16:11 +0900

    update footer content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,15 @@
 const Footer = () => {
   return (
-    <footer class="px-4 py-5 border-t-2 text-right">
-      <span>
-        Created by{" "}
-        <a href="https://github.com/taga3s" class="text-cyan-600 underline">
-          taga3s
-        </a>
-      </span>
+    <footer class="px-4 py-5 border-t-2 text-right flex justify-end">
+      <div class="flex items-center gap-2">
+        <img class="w-6" src="logo.svg" alt="fresh icon" />
+        <span>
+          Created by{" "}
+          <a href="https://github.com/taga3s" class="text-cyan-600 underline">
+            taga3s
+          </a>
+        </span>
+      </div>
     </footer>
   );
 };
